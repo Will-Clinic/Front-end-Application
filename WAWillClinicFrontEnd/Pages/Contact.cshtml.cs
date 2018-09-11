5841e225fd5eb63c0235f52edb7874622b5934ab
Eric Singleton <eric.singleton.jr@gmail.com>
2018-09-11 12:54:06 -0700

    Started the contact us form

--- WAWillClinicFrontEnd/Pages/Contact.cshtml.cs
+++ WAWillClinicFrontEnd/Pages/Contact.cshtml.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,10 +9,39 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WAWillClinicFrontEnd.Pages
 {
+    [BindProperties]
     public class ContactModel : PageModel
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        public ContactType Reason { get; set; }
+
         public void OnGet()
         {
+
         }
+
+        public void OnPost()
+        {
+
+        }
+
+        /// <summary>
+        /// Used to create a reason select box that is easier to add or remove
+        /// future reasons
+        /// </summary>
+        public enum ContactType
+        {
+            [Display(Name = "I have a question about an upcoming clinc")]
+            Upcoming,
+            [Display(Name = "I'd like to donate")]
+            Donate,
+            [Display(Name = "I am with the media and have questions")]
+            Media,
+            [Display(Name = "other")]
+            Other
+        };
     }
 }
