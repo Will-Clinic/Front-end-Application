9b96c6959a832f90d6c5af0a2b01113a93608eb0
jcqnly <jcqnly@gmail.com>
2018-09-14 09:44:11 -0700

    create js folder, add jQuery CDN, move JS code to js file

---
+++ WAWillClinicFrontEnd/wwwroot/js/app.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+$(document).ready(() => {
+	$('.fa-bars').on('click', () => {
+		$('header').slideDown();
+	});
+
+	$('.fa-times').on('click', () => {
+		$('header').hide();
+	});
+})
