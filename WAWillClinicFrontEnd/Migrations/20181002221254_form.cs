82db9f14d3212a046559d05f5e3cb9cfa6097ea5
jcqnly <jcqnly@gmail.com>
2018-10-02 16:05:25 -0700

    add 1/3rd of questions

---
+++ WAWillClinicFrontEnd/Migrations/20181002221254_form.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WAWillClinicFrontEnd.Migrations
+{
+    public partial class form : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
