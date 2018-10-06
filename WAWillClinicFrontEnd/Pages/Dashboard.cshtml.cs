69d7ea1a2ba41e43d8354bb507de2063ac09dbd4
ecaoile <unfie555@gmail.com>
2018-10-06 13:50:17 -0700

    working search

--- WAWillClinicFrontEnd/Pages/Dashboard.cshtml.cs
+++ WAWillClinicFrontEnd/Pages/Dashboard.cshtml.cs
@@ -18,19 +18,31 @@ namespace WAWillClinicFrontEnd.Pages
     {
         private UserDbContext _context;
         public List<RSVPUser> Users { get; set; } = new List<RSVPUser>();
-
         public DashboardModel(UserDbContext context)
         {
             _context = context;
         }
         /// <summary>
         /// Action that grabs all of our registered users within the
-        /// database
+        /// database or a subset of users based on a search for name
         /// </summary>
         /// <returns>Page</returns>
-        public async Task OnGet()
+        public async Task OnGet(string searchString)
         {
-            Users = await _context.Users.ToListAsync();
+            // defines a base query to work with, but does not run it
+            // against the db yet
+            var users = from u in _context.Users
+                        select u;
+
+            // filter if user searches by name
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                users = _context.Users.
+                    Where(u => u.Name.ToLower().Contains(searchString.ToLower()));
+            }
+
+            // formats query (or default list) into a list format to display on the page
+            Users = await users.ToListAsync();
         }
     }
 }
