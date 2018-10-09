48e1b303ca1152189da2e69e253f252e8f49304c
Eric Singleton <eric.singleton.jr@gmail.com>
2018-10-08 17:34:58 -0700

    Added tests for custom fields in ApplicationUser

---
+++ FrontEndTests/ModelTests/ApplicationUserCustomGetterSetterTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+using WAWillClinicFrontEnd.Models;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrontEndTests.ModelTests
+{
+    public class ApplicationUserCustomGetterSetterTests
+    {
+        [Fact]
+        public void FirstNameGetterAndSetter()
+        {
+            ApplicationUser user = new ApplicationUser
+            {
+                FirstName = "Bob"
+            };
+            Assert.Equal("Bob", user.FirstName);
+            user.FirstName = "Steve";
+            Assert.Equal("Steve", user.FirstName);
+        }
+        [Fact]
+        public void LastNameGetterAndSetter()
+        {
+            ApplicationUser user = new ApplicationUser
+            {
+                LastName = "Doe"
+            };
+            Assert.Equal("Doe", user.LastName);
+            user.LastName = "Smith";
+            Assert.Equal("Smith", user.LastName);
+        }
+    }
+}
