81d92f608428f716e741089dda7806eeeb58c79f
Eric Singleton <eric.singleton.jr@gmail.com>
2018-10-08 17:54:35 -0700

    Added tests for logout page model

---
+++ FrontEndTests/LogOutTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Xunit;
+using WAWillClinicFrontEnd.Pages;
+using FrontEndTests.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEndTests
+{
+    public class LogOutTests
+    {
+        [Fact]
+        public void LogOutFunctionsProperly()
+        {
+            MockStoreContexts mock = new MockStoreContexts();
+            LogoutModel lm = new LogoutModel(mock.SignInManager.Object);
+
+            mock.SignInManager.Setup(x => x.SignOutAsync())
+                .Returns(Task.CompletedTask);
+
+            var result = lm.OnGet().Result;
+            RedirectToPageResult check = (RedirectToPageResult)result;
+            Assert.Equal("/", check.PageName);
+        }
+    }
+}
