49778da5dc13166aebd6388cc6c5c23e1d0d743a
Eric Singleton <eric.singleton.jr@gmail.com>
2018-10-09 15:44:39 -0700

    Updated default password reroute, Fixed Admintests

--- WAWillClinicFrontEnd/Pages/AdminLogin.cshtml.cs
+++ WAWillClinicFrontEnd/Pages/AdminLogin.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -16,18 +17,23 @@ namespace WAWillClinicFrontEnd.Pages
     [BindProperties]
     public class AdminLoginModel : PageModel
     {
+        //Our DI fields
         private UserManager<ApplicationUser> _userManager;
         private SignInManager<ApplicationUser> _signInManager;
+        private IConfiguration _configuration;
+
         [Required]
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
 
         public AdminLoginModel(UserManager<ApplicationUser> userManager, 
-                               SignInManager<ApplicationUser> signInManager)
+                               SignInManager<ApplicationUser> signInManager,
+                               IConfiguration configuration)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _configuration = configuration;
         }
 
         public void OnGet() { }
@@ -37,11 +43,17 @@ namespace WAWillClinicFrontEnd.Pages
         /// <returns>Either a page or a Redirect to another page</returns>
         public async Task<IActionResult> OnPost()
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
+                var defaultPass = _configuration;
                 var result = await _signInManager.PasswordSignInAsync(Email, Password, false, true);
                 if(result.Succeeded)
                 {
+                    if(Password == _configuration["DFP"])
+                    {
+                        return RedirectToPage("/Change_Password");
+                    }
+
                     var user = await _userManager.FindByEmailAsync(Email);
                     if(await _userManager.IsInRoleAsync(user, ApplicationRoles.Admin))
                     {
