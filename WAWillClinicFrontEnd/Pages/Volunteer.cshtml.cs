4d5aeeea6528973f0f7b48c8d8f6a2ff82bd0f2e
Jon Rice <revyolution1120@gmail.com>
2019-08-12 12:48:36 -0700

    Volunteer model.

--- WAWillClinicFrontEnd/Pages/Volunteer.cshtml.cs
+++ WAWillClinicFrontEnd/Pages/Volunteer.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,41 @@ namespace WAWillClinicFrontEnd.Pages
 {
     public class VolunteerModel : PageModel
     {
+        public City VolunteerCity { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phonenumber { get; set; }
+
+        public Profession VolunteerProfession { get; set; }
+
+        public string Time { get; set; }
+
+        public string Pairing { get; set; }
+
+        public string Comments { get; set; }
+
+        public enum City
+        {
+
+        }
+
+        public enum Profession
+        {
+            [Display(Name = "Pasco(Tri-Cities")]
+            Pasco,
+            [Display(Name ="Wenatchee")]
+            Wenatchee,
+            [Display(Name ="Seattle(North Sound")]
+            Seattle,
+            [Display(Name ="Kitsap/Bremerton")]
+            Kitsap
+    }
+
+
         public void OnGet()
         {
         }
