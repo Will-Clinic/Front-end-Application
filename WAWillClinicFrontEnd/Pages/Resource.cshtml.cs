17e51ddd103a540ed394f86f58765aa1f2f2d744
richard0923 <r.rosadoocasio@gmail.com>
2019-08-13 15:10:15 -0700

    adding a resouce page

---
+++ WAWillClinicFrontEnd/Pages/Resource.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WAWillClinicFrontEnd.Pages
+{
+    public class ResourceModel : PageModel
+    {
+        public void OnGet()
+        {
+            // on get i need to grab all of the resources from the Db using a interface to that table.
+        }
+    }
+}
