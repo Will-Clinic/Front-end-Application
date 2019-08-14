d16b9d6a2ffa9fbf569d3587dc8524f233afb8e0
richard0923 <r.rosadoocasio@gmail.com>
2019-08-13 20:09:57 -0700

    added interfaces and Service still need to add functionality

---
+++ WAWillClinicFrontEnd/Models/Interfaces/IResource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static WAWillClinicFrontEnd.Models.Resource;
+
+namespace WAWillClinicFrontEnd.Models.Interfaces
+{
+    interface IResource
+    {
+        //Create
+        Task CreateResource(Resource resource);
+
+        //GrabAll
+        Task<List<Resource>> GetAllResources();
+
+        //GrabAllByType
+        Task<List<Resource>> GetAllResourcesByType(ResourceType type);
+
+        //GrabByID
+        Task<Resource> GetResourceById(int id);
+
+        //update
+        Task UpdateResource(Resource resource);
+
+        //Delete
+        Task DeleteResource(int id);
+    }
+}
---
+++ WAWillClinicFrontEnd/Models/Services/ResourceManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WAWillClinicFrontEnd.Models.Interfaces;
+using static WAWillClinicFrontEnd.Models.Resource;
+
+namespace WAWillClinicFrontEnd.Models.Services
+{
+    public class ResourceManager : IResource
+    {
+        /// <summary>
+        /// Creates a Resource and Saves it to the Database
+        /// </summary>
+        /// <param name="resource"></param>
+        /// <returns></returns>
+        public Task CreateResource(Resource resource)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Deletes a Resource and Saves Changes to the Database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Task DeleteResource(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets All of the Resources from the Database
+        /// </summary>
+        /// <returns>Returns a List of all those resources</returns>
+        public Task<List<Resource>> GetAllResources()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Grabs all the Resources of the same ResourceType
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns>Returns a List of all those resources</returns>
+        public Task<List<Resource>> GetAllResourcesByType(ResourceType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Grabs the Resource by id  
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Returns the Resource if found</returns>
+        public Task<Resource> GetResourceById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Updates the Resource if it's in the Database
+        /// </summary>
+        /// <param name="resource"></param>
+        /// <returns></returns>
+        public Task UpdateResource(Resource resource)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
