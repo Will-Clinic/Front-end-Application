3ffd0f3710d1d5f8b07630bc269f234566c8df8e
Jon Rice <revyolution1120@gmail.com>
2019-09-12 11:36:41 -0700

    Moved duplicate pages into PagesToBeRemoved folder. Attempted to add in admin login to db. Added Admin folder with register page to create admins with proper claim/role attached.

---
+++ WAWillClinicFrontEnd/Migrations/20190912174231_AdminData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WAWillClinicFrontEnd.Migrations.ApplicationDb
+{
+    public partial class AdminData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "fd4a5b5f-c287-4d60-8671-1d9bc923aee1", 0, "e08c48d4-2788-4071-97ec-6105b2e90179", "admin@admin.com", false, "Admin", "Test", false, null, null, null, null, null, false, null, false, null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumns: new[] { "Id", "ConcurrencyStamp" },
+                keyValues: new object[] { "fd4a5b5f-c287-4d60-8671-1d9bc923aee1", "e08c48d4-2788-4071-97ec-6105b2e90179" });
+        }
+    }
+}
